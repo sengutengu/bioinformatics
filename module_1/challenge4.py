09e197dab298ec2c751c028aefe95eacf3c24f2d
sengutengu <seungkim.h@gmail.com>
2025-01-16 16:51:13 -0500

    Add part of Week 2 content

--- module_1/challenge4.py
+++
@@ -1,9 +0,0 @@
-from bioinformatics_module import pattern_match, space_join
-
-f = open("Vibrio_cholerae.txt", "r")
-genome = f.read()
-f.close()
-
-pattern = "ATGATCAAG"
-
-print(space_join(pattern_match(genome, pattern)))
